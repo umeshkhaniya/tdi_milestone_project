af65e4f79bcc0149428286e3afc08352cd9ca967
UMESH KHANIYA <umeshkhaniya@Umeshs-MacBook-Pro-2.local>
2022-01-03 21:25:10 -0500

    initial commit

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"michaelc@thedataincubator.com\"\n\
+email = \"umesh.khaniya2010@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
